195b0d397fef32eecb971f918d87a6dfb7422f31
Robert Gingras <developer@three-point-five.dev>
2024-11-02 20:08:14 -0400

    basic, non-functional backend

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+def get_recs(decklist):
+    pass
+
+@app.get("/")
 def index():
     return render_template("index.html")
+
+@app.post("/")
+def process():
+    if request.files["decklistFile"].content_length > 0:
+        recs = get_recs(request.files["decklistFile"].read())
+    else:
+        recs = get_recs(request.form["decklist"])
+    return redirect('/')
